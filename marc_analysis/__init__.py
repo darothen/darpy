473e5438f2980da7398a52932c3f16088e880245
Daniel Rothenberg <darothen@mit.edu>
2016-04-04 12:30:05 -0400

    MAINT: Alphabetize main imports

--- marc_analysis/__init__.py
+++ marc_analysis/__init__.py
@@ -10,14 +10,15 @@ from __future__ import absolute_import, division
 # Set up some default imports to bring some of the
 # tools to the marc_aie namespace.
 # from . case_setup import *
-from . scripting import *
+from . analysis import *
+from . convert import *
 from . experiment import *
 from . extract import *
 from . io import *
-from . var import *
-from . convert import *
+from . regions import *
+from . scripting import *
 from . utilities import *
-from . analysis import *
+from . var import *
 
 try:
     from . plot import *
