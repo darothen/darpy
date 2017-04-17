db829049aed4d846c7635ee29277a3b4524c7dfa
darothen <darothen@mit.edu>
2017-04-17 15:35:22 -0400

    Updating shift_lons function to act on Datasets and DataArrays

--- darpy/utilities.py
+++ darpy/utilities.py
@@ -305,13 +305,15 @@ def cyclic_dataarray(da, coord='lon'):
 
     return new_da
 
-def shift_lons(lons):
+def shift_lons(ds, lon_dim='lon'):
     """ Shift longitudes from [0, 360] to [-180, 180] """
-    new_lons = lons.copy()
+    lons = ds[lon_dim].values
+    new_lons = np.empty_like(lons)
     mask = lons > 180
     new_lons[mask] = -(360. - lons[mask])
     new_lons[~mask] = lons[~mask]
-    return new_lons
+    ds[lon_dim].values = new_lons
+    return ds
 
 
 def area_grid(lon, lat, asarray=False):
